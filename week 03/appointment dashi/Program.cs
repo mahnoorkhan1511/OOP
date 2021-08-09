a9f17db20a80350f49b32c6fb94f2487bd327c18
mahnoorkhan1511 <mahnoor1511@gmail.com>
2021-08-09 23:29:34 +0500

    save

--- week 03/appointment dashi/Program.cs
+++ week 03/appointment dashi/Program.cs
@@ -13,12 +13,44 @@ namespace appointment_dashi
             this.name = name;
             this.pin = pin;
         }
-        public void addTreatment(string name, int charges){
+        public bool treatmentIs(string name){
             for(int i = 0; i<treatments.Count ; i++){
-                if(treatments[i].name == name){
-                   
+                if( treatments[i].name == name ){
+                    return true;
                 }
-            } 
+            }
+            return false;
+        }
+        public bool patCheck(string date){
+            for( int i = 0; i< patRecord.Count ; i++){
+                if( patRecord[i].date == date ){
+                    return true;
+                }
+            }
+            return false;
+        }
+        public void addTreatment(string name, int charges){
+            Treatments t = new Treatments(name,charges);
+            treatments.Add(t);
+        }
+        public void addPatient(string name, string date, string treatment, int moneyPayable){
+            Patient p = new Patient(name,date,treatment,moneyPayable);
+            patRecord.Add(p);
+        }
+        public void addMoney(int money ){
+            this.moneyEarned = this.moneyEarned + money;
+        }
+        public void viewAppointments(){
+            Console.WriteLine("Name" + "\t" + "Date" + "\t" + "Treatment" + "Money");
+            for( int i = 0 ; i < patRecord.Count ; i++){
+                Console.WriteLine(patRecord[i].name + "\t" + patRecord[i].date + "\t" + patRecord[i].treatment + "\t" + patRecord[i].moneyPayable);
+            }
+        }
+        public void viewTreatments(){
+            Console.WriteLine("sr." +"\t" + "Name" + "\t" + "Payment");
+            for(int i =0; i < treatments.Count ; i++){
+                Console.WriteLine(i + "\t" + treatments[i].name + "\t" + treatments[i].charges);
+            }
         }
     }
     class Patient{
@@ -45,7 +77,74 @@ namespace appointment_dashi
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Doctor doc = new Doctor("xyz", 1234);
+            Program p = new Program();
+            int option = 0;
+            while( option != 3 ){
+                option = p.mainPage();
+                if(option == 1){
+                    Console.Clear();
+                    Console.WriteLine("Verify your self by entering your pin ...");
+                    int pin = int.Parse(Console.ReadLine());
+                    if( pin == doc.pin){
+                        int opt= 0;
+                        while( opt == 7){
+                            opt = p.docPage();
+                            if( opt == 1){
+                                Console.Clear();
+                                Console.WriteLine("  Appointments ");
+                                Console.WriteLine("_________________");
+                                Console.Write("\n");
+                                doc.viewAppointments();           
+                            }
+                            
+                        }
+                    }    
+                }else if( option == 2){
+                    int opt = 0;
+                    while(opt != 5){
+                        opt = p.patPage();
+                    }
+                }
+            }
         }
+        int mainPage(){
+            Console.Clear();
+            Console.WriteLine("Enter your ID ..");
+            Console.WriteLine(" 1. Doctor ");
+            Console.WriteLine(" 2. Patient");
+            Console.WriteLine(" 3. Exit ");
+            int opt = int.Parse(Console.ReadLine());
+            return opt;    
+        }
+        int docPage(){
+            Console.Clear();
+            Console.WriteLine(" Welcome Doctor ..! ");
+            Console.Write("\n");
+            Console.WriteLine("Enter your option ..");
+            Console.WriteLine(" 1. View appointments ");
+            Console.WriteLine(" 2. Create appointment ");
+            Console.WriteLine(" 3. Cancel Appointment ");
+            Console.WriteLine(" 4. View treatments ");
+            Console.WriteLine(" 5. Add treatment ");
+            Console.WriteLine(" 6. Total money earned ");
+            Console.WriteLine(" 7. exit");
+            int opt = int.Parse(Console.ReadLine());
+            return opt;
+        }
+        int patPage(){
+            Console.Clear();
+            Console.WriteLine(" Welcome Sir/ Ma'am ..!");
+            Console.Write("\n");
+            Console.WriteLine("Enter your option ..");
+            Console.WriteLine(" 1. Create Appointment ");
+            Console.WriteLine(" 2. Cancel Appointment ");
+            Console.WriteLine(" 3. Appointment Status ");
+            Console.WriteLine(" 4. View Treatments ");
+            Console.WriteLine(" 5. Exit");
+            int opt = int.Parse(Console.ReadLine());
+            return opt;
+        }
+        
     }
 }
