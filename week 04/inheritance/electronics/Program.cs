f4ffe42400e01ff4ae319f19bde23880da95e1d3
mahnoorkhan1511 <mahnoor1511@gmail.com>
2021-08-20 22:00:58 +0500

    save

---
+++ week 04/inheritance/electronics/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace electronics
+{
+    class Electronics{
+        public string brand,color;
+        //quantity
+        int qty;
+        public Electronics(string brand, string color, int qty){
+            this.brand = brand;
+            this.color = color;
+            this.qty = qty;
+        }
+        public string properties(){
+            return "the  properties are : " + "\n" + "brand " + brand +", color " + color + ", quantity " + qty;
+        }
+    }
+    class Computer : Electronics{
+        string model;
+        public Computer(string brand, string color, int qty,string model) : base (brand, color, qty){
+            this.model = model;
+        }
+        public string viewComputer(){
+            return "Computer : " + this.model + "\n" + properties();  
+        }
+    }
+    class Laptop : Electronics{
+        string model;
+        public Laptop(string brand, string color, int qty,string model) : base (brand, color, qty){
+            this.model = model;
+        }
+        public string viewLaptop(){
+            return "Laptop : " + this.model + "\n" + properties();  
+        }
+    }
+    class SmartPhone : Electronics{
+        string model;
+        public SmartPhone(string brand, string color, int qty,string model) : base (brand, color, qty){
+            this.model = model;
+        }
+        public string viewSmartPhone(){
+            return "SmartPhone : " + this.model + "\n" + properties();  
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Computer computer = new Computer("hp", "silver" , 6 , "HP Envy 14-eb0021TX");;
+            Laptop laptop = new Laptop("hp", "silver" , 6 , "HP Envy 14-eb0021TX");
+            SmartPhone smartPhone  = new SmartPhone("Samsung" , "black", 12 , "a52");
+            computer.viewComputer();
+            laptop.viewLaptop();
+            smartPhone.viewSmartPhone();
+        }
+    }
+}
---
+++ week 04/inheritance/person/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace person
+{
+    class Person{
+        public string name = "";
+        public Person(string name){
+            this.name = name;
+        }        
+    }
+    class Student:Person{
+        public Student(string name ) : base(name)
+        {
+
+        }
+        public string getName(){
+            return name;
+        }
+        public void setName(string name)
+        {
+            this.name = name;
+        }
+        public string study(){
+            return name + ", the student studies ..";
+        }
+    }
+    class Teacher:Person{
+        public Teacher(string name ) : base(name)
+        {
+
+        }
+        public string getName(){
+            return name;
+        }
+        public void setName(string name)
+        {
+            this.name = name;
+        }
+        public string explain(){
+            return name + ", the teacher explains ..";
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s = new Student("Drake");
+            Teacher t = new Teacher("Namjoon");
+            t.explain();
+            s.study();
+        }
+    }
+}
